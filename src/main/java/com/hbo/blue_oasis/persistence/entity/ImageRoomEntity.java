7558299bdd689a4de9ea914934fcfd916da0e3cf
romelmain <romelherreratgk@gmail.com>
2025-03-06 22:09:56 -0400

    Create Entities: RoomEntity and ImageRoomEntity

---
+++ src/main/java/com/hbo/blue_oasis/persistence/entity/ImageRoomEntity.java
@@ -0,0 +1,46 @@
+package com.hbo.blue_oasis.persistence.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "image_room")
+public class ImageRoomEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String image;
+
+    @Column(name = "main_image", nullable = false, updatable = false)
+    private Boolean mainImage;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_at")
+    private Date createAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "update_at")
+    private Date updateAt;
+
+    @Column(name = "room_id")
+    private int roomId;
+
+}
---
+++ src/main/java/com/hbo/blue_oasis/persistence/entity/RoomEntity.java
@@ -0,0 +1,53 @@
+package com.hbo.blue_oasis.persistence.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@ToString
+@Entity
+@Table(name = "room")
+public class RoomEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, updatable = false)
+    private Boolean availability;
+
+    private BigDecimal price;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_at")
+    private Date createAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "update_at")
+    private Date updateAt;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private List<ImageRoomEntity> imageRoom;
+
+}
