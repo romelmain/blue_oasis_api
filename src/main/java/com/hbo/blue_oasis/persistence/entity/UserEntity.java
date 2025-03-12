0559f739f856f25eb06866056b425630f9992596
romelmain <romelherreratgk@gmail.com>
2025-03-11 22:54:45 -0400

    Create Guest Entity

--- src/main/java/com/hbo/blue_oasis/persistence/entity/UserEntity.java
+++ src/main/java/com/hbo/blue_oasis/persistence/entity/UserEntity.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.*;
 
@@ -51,5 +52,8 @@ public class UserEntity implements Serializable {
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<RoleEntity> roles = new HashSet<>();
+
+    @OneToOne(mappedBy = "userEntity")
+    GuestEntity guestEntity;
 
 }
